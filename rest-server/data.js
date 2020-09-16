c9eb71369e16d8d41df29673053b9d7a8d27913a
Om Gupta <omgupta1608@gmail.com>
2020-09-16 22:16:32 +0530

    rest server coded

---
+++ rest-server/data.js
@@ -0,0 +1,18 @@
+module.exports.data = [
+    {
+        id: 1,
+        name: "Sahil"
+    },
+    {
+        id: 2,
+        name: "Rishi" 
+    },
+    {
+        id: 3,
+        name: "Samridhi"
+    },
+    {
+        id: 4,
+        name: "Om"
+    }
+];
--- rest-server/index.js
+++ rest-server/index.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
+var data = require('./data').data;
 
 const app = express();
-
+morgan(':remote-addr :method :url');
 app.use(express.json());
 
+app.get('/', (req,res) => {
+    res.send(data);
+});
+app.get('/:id', (req,res) => {
+    var user = null;
+    for(var i = 0; i < data.length; i++)
+    {
+        if(data[i].id == req.params.id)
+        {
+            user = data[i];
+        }
+    }
+    res.send(user);
+});
+
+app.post('/addUser', (req,res) => {
+    var newUser = {
+        id: data.length + 1,
+        name: req.body.name
+    };
+
+    data.push(newUser);
+
+    res.send('User Added');
+});
 app.listen(4000, () => {
     console.log('Rest Server Started');
 });
